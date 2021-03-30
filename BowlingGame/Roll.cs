d49115918ead0bbe34271931ef38bdcd33b2448a
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-03-30 09:59:16 +0200

    implemented open frame case

---
+++ BowlingGame/Roll.cs
@@ -0,0 +1,12 @@
+﻿namespace BowlingGame
+{
+    public class Roll
+    {
+        public Roll(in int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+    }
+}
